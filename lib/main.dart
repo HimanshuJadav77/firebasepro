16b18358aca31cbbef2a0b9f6a328c03be62e001
Admin <bigbhai1wr@gmail.com>
2024-06-21 08:45:11 +0530

    title:SUKUNA

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Yuji Itadori'),
+      home: const MyHomePage(title: 'SUKUNA ITADORI'),
     );
   }
 }
